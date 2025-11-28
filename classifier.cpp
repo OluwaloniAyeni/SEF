dce2380881372fc261a14ab8b0c2f42b4aac05d9
Oluwaloni <ayenimidun@gmail.com>
2025-11-27 20:07:19 -0500

    Edited train() on main

--- classifier.cpp
+++ classifier.cpp
@@ -1,10 +1,9 @@
 #include <stdio.h>
 void train() {
-printf("train\n")
-printf("The first feature is added\n")
+printf("train\n");
+printf("MAIN VERSION HERE\n");
 }
 void predict() {
-printf("predict\n")
-printf("The second feature is added\n")
+printf("predict\n");
+printf("The second feature is added\n");
 }
-
