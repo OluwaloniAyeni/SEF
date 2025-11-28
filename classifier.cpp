34f0446fd753660aa9100debbc012b55e1031e3a
Oluwaloni <ayenimidun@gmail.com>
2025-11-27 20:14:53 -0500

    Edited train() differently on secondFeature

--- classifier.cpp
+++ classifier.cpp
@@ -1,10 +1,10 @@
 #include <stdio.h>
 void train() {
-printf("train\n")
-printf("The first feature is added\n")
+printf("train\n");
+printf("SECOND FEATURE VERSION HERE\n");
 }
 void predict() {
-printf("predict\n")
-printf("The second feature is added\n")
+printf("predict\n");
+printf("The second feature is added\n");
 }
 
